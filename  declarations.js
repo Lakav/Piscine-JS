789fcddecfb351164c86ce81b88f9dd53f6b3bcf
julescavanier <jules.cavanier@ynov.com>
2022-03-25 17:06:54 +0100

    ex3

---
+++  declarations.js
@@ -0,0 +1,17 @@
+escape = "\'\\\/\"\`"
+let arr = [4,"2"]
+
+let obj = {
+    str : 'string'
+    num : 5
+    bool : true
+    undef : undefined
+}
+let nested = {
+    arr : [4,undefined,'2']
+    obj = {
+        str : 'string'
+        num : 5
+        bool : true
+    }
+}
