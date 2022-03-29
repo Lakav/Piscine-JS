9dedff888a4b2e46335db73384caa9f9abe57849
julescavanier <jules.cavanier@ynov.com>
2022-03-29 14:00:49 +0200

    ok

---
+++ declarations.js
@@ -0,0 +1,21 @@
+escape = "\'\\\/\"\`"
+let arr = [4,"2"]
+
+let obj = {
+    str : 'string',
+    num : 5,
+    bool : true,
+    undef : undefined
+}
+let nested = {
+    arr : [4,undefined,'2'],
+    obj = {
+        str : 'string',
+        num : 5
+        bool : true
+    }
+}
+Object.freeze(obj)
+Object.freeze(arr)
+Object.freeze(nested.obj)
+Object.freeze(nested.arr)
