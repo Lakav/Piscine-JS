d0dfed235cab055d077b5016cbb123b908a770c3
julescavanier <jules.cavanier@ynov.com>
2022-04-01 15:46:34 +0200

    ok

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more (a){
+    console.log(arguments[0]+1)
+}
+function less (b){
+    console.log(arguments[0]-1)
+}
+function add (c){
+    console.log(arguments[0]+arguments[0])
+}
+function sub (d){
+    console.log(arguments[0]-arguments[0])
+}
