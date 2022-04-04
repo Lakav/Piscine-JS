f7eaff96fa3c0af195fcbdea2106686e97636fdd
julescavanier <jules.cavanier@ynov.com>
2022-04-04 16:28:14 +0200

    ok

---
+++ method-man.js
@@ -0,0 +1,15 @@
+function word(a){
+    return a.split(" ")
+}
+function sentence(b){
+    return b.join(" ")
+}
+function yell(c){
+    return c.toUpperCase()
+}
+function whisper(d){
+    return "*"+d.toLowerCase()+"*"
+}
+function capitalize(e){
+    return e[0].toUpperCase() + e.slice(1).toLowerCase()
+}
