16af0be0bda5503c56b2e77e4da99887ca56e8cc
julescavanier <jules.cavanier@ynov.com>
2022-04-05 14:17:22 +0200

    ok

---
+++ min-max.js
@@ -0,0 +1,22 @@
+function max (a,b){
+    if(a>b){
+        return a
+    }
+    if(b<a){
+        return b
+    }
+    else{
+        return "same value"
+    }
+}
+function min (c,d){
+    if(c>d){
+        return d
+    }
+    if(c<d){
+        return c
+    }
+    else{
+        return "same value"
+    }
+}
