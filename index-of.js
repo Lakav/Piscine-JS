a8b4cc20f922e93fb6bd537d7a3d710e3798bfcd
julescavanier <jules.cavanier@ynov.com>
2022-04-06 14:38:53 +0200

    ok

--- index-of.js
+++ index-of.js
@@ -6,4 +6,12 @@ function indexOf(arr,value,pos=0){
     }
 return -1
 }
-console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
+function lastIndexOf(arr,value,pos=arr.length){
+    for(let i=pos; i>0; i--){
+        if(arr[i] == value){
+            return i
+        }
+    }
+return -1
+}
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
