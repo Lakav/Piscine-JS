46f281b2e649d883ba41051cfd28862d10f8dcb6
julescavanier <jules.cavanier@ynov.com>
2022-04-20 17:07:27 +0200

    ok

--- cut-corners.js
+++ cut-corners.js
@@ -1,9 +1,40 @@
+function modulo (a,b){
+    let res = 0
+    if(a>0 && b>0){
+        for(res; a>b ;res++){
+            a-=b
+        }
+    }
+    if(a<0 && b<0){
+        a = -a
+        b = -b
+        for(res; a>b ;res++){
+            a-=b
+        }
+        a = -a
+    }
+    if(a<0){
+        a = -a
+        for(res; a>b ;res++){
+            a-=b
+        }
+        a = -a
+    }
+    if(b<0){
+        b = -b
+        for(res; a>b ;res++){
+            a-=b
+        }
+    }
+    return a
+}
+
 function round(num){
 
     if(num < 0){
 
         let numPositive = num * -1
-        let mod = numPositive%1
+        let mod = modulo(numPositive, 1)
 
         if (mod < 0.5){
             numPositive = numPositive - mod
@@ -15,7 +46,7 @@ function round(num){
         }
 
     }
-    let mod = num % 1 
+    let mod = modulo(num, 1)
     if (mod < 0.5){
         return num - mod
     }
@@ -27,7 +58,7 @@ function round(num){
 function ceil(num){
     if(num < 0){
         let numPositive = num * -1
-        let mod = numPositive%1
+        let mod = modulo(numPositive, 1)
 
         return (numPositive - mod)* -1
     }
@@ -35,14 +66,14 @@ function ceil(num){
         return 0
     }
 
-    let mod = num%1
+    let mod = modulo(num, 1)
     return num - mod + 1
 }
 
 function floor(num){
     if(num < 0){
         let numPositive = num * -1
-        let mod = numPositive%1
+        let mod = modulo(numPositive, 1)
 
         return (numPositive - mod+1)* -1
     }
@@ -50,20 +81,20 @@ function floor(num){
         return 0
     }
 
-    let mod = num%1
+    let mod = modulo(num, 1)
     return num - mod
 }
 
 function trunc(num){
     if (num < 0){
         let numPositive = num * -1
-        let mod = numPositive%1
+        let mod = modulo(numPositive, 1)
 
         return (numPositive - mod)* -1
     }
 
     else {
-        let mod = num%1
+        let mod = modulo(num, 1)
         return num - mod
     }
 }
